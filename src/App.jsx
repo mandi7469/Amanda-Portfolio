64d2200241386c61aa24f58c9bcefd3aa08f09f0
mandi7469 <mandi7469@aol.com>
2024-09-17 16:18:49 -0400

    added resume component and related json data

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import styles from "./App.module.css";
+import { Resume } from "./components/Resume/Resume";
 import { About } from "./components/About/About";
 import { Hero } from "./components/Hero/Hero";
-import  Navbar  from "./components/Navbar/Navbar";
+import Navbar from "./components/Navbar/Navbar";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <>
-     
       <div className={styles.App}>
-     
         <Navbar />
         <Hero />
         <About />
+        <Resume />
       </div>
     </>
   );
