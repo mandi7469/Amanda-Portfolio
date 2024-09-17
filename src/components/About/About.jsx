039f0cb0272913ba049a6980665c0bbdaefa4e93
mandi7469 <mandi7469@aol.com>
2024-09-17 15:19:11 -0400

    added experience png logo icons

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,6 +6,10 @@ export const About = () => {
   return (
     <section className={styles.container} id="about">
       <h2 className={styles.title}>About</h2>
+      <p className={styles.aboutP}>
+        In addition to my passion for coding, I love to spend my free time
+        playing volleyball, kickball, and disc golf{" "}
+      </p>
       <div className={styles.content}>
         <img
           src={getImageUrl("about/about.png")}
@@ -21,26 +25,34 @@ export const About = () => {
             />
             <div className={styles.aboutItemText}>
               <h3>Front-End Developer</h3>
-              <p>Experience in building responsive and optimized applications</p>
-            </div>
-          </li>
-          <li className={styles.aboutItem}>
-            <img src={getImageUrl("about/serverIcon.png")} alt="Server-icon by Icons8" />
-            <div className={styles.aboutItemText}>
-              <h3>Back-End Developer</h3>
               <p>
-                Experience developing fast and optimised back-end systems
-                and APIs
+                Experience in building responsive and optimized applications
               </p>
             </div>
           </li>
           <li className={styles.aboutItem}>
-            <img src={getImageUrl("about/designIcon.png")} alt="web-design-icon by Icons8" />
+            <img
+              src={getImageUrl("about/serverIcon.png")}
+              alt="Server-icon by Icons8"
+            />
+            <div className={styles.aboutItemText}>
+              <h3>Back-End Developer</h3>
+              <p>
+                Experience in building high-performance back-end systems and
+                APIs
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img
+              src={getImageUrl("about/designIcon.png")}
+              alt="web-design-icon by Icons8"
+            />
             <div className={styles.aboutItemText}>
               <h3>UI Designer</h3>
               <p>
-                Experience designing multiple landing pages and design
-                systems
+                Deep understanding of user experience principles and visual
+                design best practices
               </p>
             </div>
           </li>
