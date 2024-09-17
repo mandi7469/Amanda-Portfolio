3c53bbe113a67de230e2c792c75405f59f3b44b1
mandi7469 <mandi7469@aol.com>
2024-09-17 00:04:45 -0400

    added background image and ui edits

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import { Hero } from "./components/Hero/Hero";
 import  Navbar  from "./components/Navbar/Navbar";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+
 function App() {
   return (
     <>
+     
       <div className={styles.App}>
+     
         <Navbar />
         <Hero />
+        
       </div>
     </>
   );
