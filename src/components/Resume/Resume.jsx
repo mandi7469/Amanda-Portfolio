07fd98ce197b673b4b0ad4b63a7d52186b4d286c
mandi7469 <mandi7469@aol.com>
2024-09-19 12:16:17 -0400

    completed resume section and styles

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -1,7 +1,56 @@
-import React from 'react'
+import React from "react";
+import styles from "./Resume.module.css";
+import skills from "../data/skills.json";
+import history from "../data/history.json";
+import pdf from "../../../assets/history/AmandaChangaResume.pdf";
+import { getImageUrl } from "../../utils";
 
 export const Resume = () => {
   return (
-    <div>Resume</div>
-  )
-}
+    <section className={styles.container} id="resume">
+      <h2 className={styles.title}>Resume</h2>
+      <a
+        href={pdf}
+        download="AmandaChangaResume.pdf"
+        style={{ color: "#9eefe5", textDecoration: "none" }}
+      >
+        <i className="bi bi-file-earmark-text p-1"></i>Download
+      </a>
+      <div className={styles.content}>
+        <div className={styles.skills}>
+          {skills.map((skill, id) => {
+            return (
+              <div key={id} className={styles.skill}>
+                <div className={styles.skillImageContainer}>
+                  <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
+                </div>
+                <p>{skill.title}</p>
+              </div>
+            );
+          })}
+        </div>
+        <ul className={styles.history}>
+          {history.map((historyItem, id) => {
+            return (
+              <li key={id} className={styles.historyItem}>
+                <div className={styles.historyItemDetails}>
+                  <img
+                    src={getImageUrl(historyItem.imageSrc)}
+                    alt={`${historyItem.organisation} Logo`}
+                  />
+                  <h3>{`${historyItem.role}, ${historyItem.organisation}`}</h3>
+                  <p>{`${historyItem.startDate} - ${historyItem.endDate}`}</p>
+                  <ul>
+                    {historyItem.experiences.map((experience, id) => {
+                      return <li key={id}>{experience}</li>;
+                    })}
+                  </ul>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </section>
+  );
+};
